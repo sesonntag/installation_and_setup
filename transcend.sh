1850104f3a1109c0de3a0e178fafa6165cd98183
Sebastian Sonntag <se.bastian.sonntag@hotmail.com>
2017-05-14 22:35:14 +0200

    wrote list of transcend links to be created

--- transcend.sh
+++ transcend.sh
@@ -8,5 +8,25 @@
 # License:
 #**********************************************************************************************
 
-# Aktuelles path
-ln -s ~/Library/Mobile\ Documents/com~apple~CloudDocs ~/iCloud
+# collection of pictures path
+ln -s ~/Volumes/Transcend/Bilder/Sammlung ~/Pictures/Sammlung
+
+# 
+ln -s ~/Volumes/Transcend/Bilder/SSW ~/Pictures/SSW
+
+# not directly related to trancend but eiter way: flat documents linked into Dropbox
+rm -r ~/Dropbox/Laura\ und\ Bastian/Wohnung
+ln -s ~/Dokumente/Wohnung ~/Dropbox/Laura\ und\ Bastian/Wohnung
+
+# movie links
+ln -s /Volumes/Transcend/Clips ~/Movies/Clips
+ln -s /Volumes/Transcend/Videos ~/Movies/Videos
+
+# music links
+ln -s /Volumes/Transcend/iTunes ~/Music/iTunes
+
+# macOS software path
+ln -s /Volumes/Transcend/System/macOS/Software/ ~/iCloud/System/Apple/macOS/Software
+
+# university documents
+ln -s /Volumes/Transcend/Uni ~/Uni
