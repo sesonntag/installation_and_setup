0cfbdd470bab46dc1f6a80777fc7d1f8ed632935
Sebastian Sonntag <se.bastian.sonntag@hotmail.com>
2017-06-16 21:27:07 +0200

    added zsh and oh-my-zsh commands to my installation routine

--- install.sh
+++ install.sh
@@ -36,5 +36,9 @@ brew install speedtest_cli
 # install tree
 brew install tree
 
+# install zsh and oh-my-zsh
+brew install zsh zsh-completions
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
 # run brew doctor once again after installation
 brew doctor
