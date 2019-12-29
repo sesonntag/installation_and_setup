8b03b3e3fc718398a64dddeddc1d0bb078043d8b
Sebastian Sonntag <se.bastian.sonntag@hotmail.com>
2019-12-29 22:48:14 +0100

    Update setup_custom_zsh_themes.sh
    add powerlevel10k theme

--- multi_plattform/setup_custom_zsh_themes.sh
+++ multi_plattform/setup_custom_zsh_themes.sh
@@ -4,7 +4,7 @@
 # Title: setup_custom_zsh_themes.sh
 # Description:
 # Author: Sebastian Sonntag
-# Date: 2018-12-17
+# Date: 2019-12-29
 # License:
 #**********************************************************************************************
 
@@ -13,3 +13,6 @@ mkdir $ZSH_CUSTOM/themes
 
 # download hyperzsh theme
 wget -O $ZSH_CUSTOM/themes/hyperzsh.zsh-theme https://raw.githubusercontent.com/tylerreckart/hyperzsh/master/hyperzsh.zsh-theme
+
+# download powerlevel10k theme
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
